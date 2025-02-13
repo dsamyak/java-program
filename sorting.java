66191b6deb9d034b6c4233d4bd69a8de97809b0d
sam <you@example.com>
2025-02-13 17:23:26 +0530

    this is commit

--- sorting.java
+++ sorting.java
@@ -17,7 +17,7 @@ public class sorting{
                 }
             }
         }
-        for (int i = 0; i <a.length; i++) 
+        for (int i=0; i<a.length; i++) 
         {
             System.out.println(a[i]);
         }
